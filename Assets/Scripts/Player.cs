297f1c43ed4bf3acd3bdf6b9a9fa1cf7c75b7593
fredanjos <fred_dosanjos@hotmail.com>
2022-07-28 09:14:40 -0300

    movimentação do player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,37 +4,138 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Variaveis do Player")]
-    public CharacterController  _characterController;
-    public float                speed;
-    public float                jumpHeight;
-    public float                gravity;
-    private float               jumpVelocity;
+    //[Header("Variaveis do Player")]
+    //public CharacterController  _characterController;
+    //public float                speed;
+    //public float                jumpHeight;
+    //public float                gravity;
+    //private float               jumpVelocity;
+    //Vector3 direction;
 
-    void Start()
+    //void Start()
+    //{
+    //    _characterController = GetComponent<CharacterController>();
+    //}
+
+
+    //void Update()
+    //{
+
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    direction.z *= speed;
+    //    //direction = Vector3.forward * speed;
+
+    //    if (_characterController.isGrounded)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Space))
+    //        {
+    //            jumpVelocity = jumpHeight;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        jumpVelocity -= gravity;
+    //    }
+
+    //    direction.y = jumpVelocity;
+
+    //    _characterController.Move(direction * Time.deltaTime);
+    //}
+
+    public static Player instance;
+
+    private bool is_ground;
+    private bool is_walk;
+    private Vector3 moviment;
+    private int id_point = 1;
+    private Vector3 target_position;
+
+    public Transform power_sensor;
+    public Transform colletable_target;
+
+    public float speed;
+    public float speed_change;
+    public float jump_force;
+    public Transform ground_check;
+    public LayerMask wait_is_ground;
+    [HideInInspector]
+    public Rigidbody plr_rb;
+    public Transform[] way_points;
+
+    private void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
     }
 
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        plr_rb = GetComponent<Rigidbody>();
+        target_position = new Vector3(way_points[id_point].position.x, transform.position.y, transform.position.z);
+
+        //FadeInOut.instance.Fade();
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Vector3.forward * speed;
-
-        if (_characterController.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                jumpVelocity = jumpHeight;
-            }
-        }
-        else
-        {
-            jumpVelocity -= gravity;
+            Jump();
         }
 
-        direction.y = jumpVelocity;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            InputX(1);
+        }
 
-        _characterController.Move(direction * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            InputX(-1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        is_ground = Physics.CheckSphere(ground_check.position, 0.3f, wait_is_ground);
+
+        target_position = new Vector3(way_points[id_point].position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target_position, speed_change * Time.deltaTime);
+        //power_sensor.position = transform.position;
+        moviment = new Vector3(0, plr_rb.velocity.y, speed);
+        plr_rb.velocity = moviment;
+    }
+
+    // Meus metodos
+
+    public void Jump()
+    {
+        if (is_ground == false) { return; }
+
+        //AudioManager.instance.PlayFx(AudioManager.instance.fx_jump);
+        plr_rb.AddForce(Vector3.up * jump_force, ForceMode.Impulse);
+    }
+
+    public void InputX(int value)
+    {
+        id_point += value;
+
+        if (id_point >= way_points.Length)
+        {
+            id_point = way_points.Length - 1;
+        }
+        else if (id_point < 0)
+        {
+            id_point = 0;
+        }
     }
 }
